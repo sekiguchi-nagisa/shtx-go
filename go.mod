2a156a97b4c34459f6b5c212f50e4799dacb053f
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2022-09-27 23:59:39 +0900

    add debug dump

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module shtx
 
 go 1.18
 
-require mvdan.cc/sh/v3 v3.5.1
+require (
+	github.com/jessevdk/go-flags v1.5.0
+	mvdan.cc/sh/v3 v3.5.1
+)
+
+require golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
--- main.go
+++ main.go
@@ -3,29 +3,66 @@ package main
 import (
 	"bufio"
 	"fmt"
+	flags "github.com/jessevdk/go-flags"
+	"io"
 	"mvdan.cc/sh/v3/syntax"
 	"os"
 )
 
-func main() {
-	if len(os.Args) == 1 {
-		fmt.Fprintf(os.Stderr, "[usage] %s [script]\n", os.Args[0])
-		os.Exit(1)
-	}
+type Options struct {
+	DumpAST string `short:"d" long:"dump" description:"dump internal ast to specified file (default to stderr)" optional:"true" optional-value:"/dev/stderr"`
+	Args    struct {
+		SCRIPT string
+	} `positional-args:"yes" required:"yes"`
+}
 
-	f, e := os.Open(os.Args[1])
-	defer f.Close()
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "cannot open file: %s, caused by `%s'\n", os.Args[1], e.Error())
-		os.Exit(1)
-	}
-
-	b := bufio.NewReader(f)
-
-	file, e := syntax.NewParser().Parse(b, "")
+func dump(r io.Reader, w io.Writer) {
+	f, e := syntax.NewParser().Parse(r, "")
 	if e != nil {
 		fmt.Fprintf(os.Stderr, "[error] %s\n", e.Error())
 		os.Exit(1)
 	}
-	syntax.NewPrinter().Print(os.Stdout, file)
+	syntax.DebugPrint(w, f)
+}
+
+func translate(r io.Reader, w io.Writer) {
+	f, e := syntax.NewParser().Parse(r, "")
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "[error] %s\n", e.Error())
+		os.Exit(1)
+	}
+	syntax.NewPrinter().Print(w, f)
+}
+
+func main() {
+	options := Options{}
+	p := flags.NewParser(&options, flags.Default)
+	_, e := p.Parse()
+	if e != nil {
+		p.WriteHelp(os.Stderr)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s\n", options.DumpAST)
+	fmt.Printf("%s\n", options.Args)
+
+	f, e := os.Open(options.Args.SCRIPT)
+	defer f.Close()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "cannot open file: %s, caused by `%s'\n", options.Args.SCRIPT, e.Error())
+		os.Exit(1)
+	}
+
+	b := bufio.NewReader(f)
+	if len(options.DumpAST) == 0 {
+		translate(b, os.Stdout)
+	} else {
+		d, e := os.Open(options.DumpAST)
+		defer d.Close()
+		if e != nil {
+			fmt.Fprintf(os.Stderr, "cannot open file: %s, caused by `%s'\n", options.DumpAST, e.Error())
+			os.Exit(1)
+		}
+		dump(b, d)
+	}
 }
