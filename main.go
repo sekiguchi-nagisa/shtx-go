95ffef58cfd976a76f2dcdbebca472c44bb3e37d
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2022-10-08 01:50:09 +0900

    add --version option

--- main.go
+++ main.go
@@ -7,13 +7,15 @@ import (
 	"io"
 	"mvdan.cc/sh/v3/syntax"
 	"os"
+	"runtime/debug"
 )
 
 type Options struct {
 	DumpAST string `short:"d" long:"dump" description:"Dump internal ast to specified file (default to stderr)" optional:"true" optional-value:"/dev/stderr"`
+	Version bool   `short:"v" long:"version" description:"Show version info"`
 	Args    struct {
 		SCRIPT string
-	} `positional-args:"yes" required:"yes"`
+	} `positional-args:"yes"`
 }
 
 func dump(r io.Reader, w io.Writer) {
@@ -35,16 +37,38 @@ func translate(r io.Reader, w io.Writer) {
 	syntax.NewPrinter().Print(w, f)
 }
 
+func getVersion() string {
+	info, ok := debug.ReadBuildInfo()
+	if ok {
+		return info.Main.Version
+	} else {
+		return "(unknown)"
+	}
+}
+
 func main() {
 	options := Options{}
 	p := flags.NewParser(&options, flags.Default)
-	_, e := p.Parse()
-	if e != nil {
-		p.WriteHelp(os.Stderr)
-		os.Exit(1)
+	if _, e := p.Parse(); e != nil {
+		if flagsErr, ok := e.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
+		} else {
+			p.WriteHelp(os.Stderr)
+			os.Exit(1)
+		}
+	}
+
+	if options.Version {
+		fmt.Println(getVersion())
+		os.Exit(0)
 	}
 
 	script := options.Args.SCRIPT
+	if script == "" {
+		fmt.Fprintln(os.Stderr, "the argument `SCRIPT` was not provided")
+		p.WriteHelp(os.Stderr)
+		os.Exit(1)
+	}
 	if script == "-" {
 		script = "/dev/stdin"
 	}
